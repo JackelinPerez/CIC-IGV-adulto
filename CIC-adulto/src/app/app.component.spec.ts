89307f0d55dbb6e821c977a06b1cfeb46dd817d0
jperez <filomenochahua@gmail.com>
2019-10-28 09:19:22 -0500

    Install steps

---
+++ CIC-adulto/src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'CIC-adulto'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('CIC-adulto');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('CIC-adulto app is running!');
+  });
+});
---
+++ CIC-adulto/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCEYKDUQcKc4ruuSL-BJOeGiTaN2t69wxo",
+    authDomain: "pruebas-onp.firebaseapp.com",
+    databaseURL: "https://pruebas-onp.firebaseio.com",
+    projectId: "pruebas-onp",
+    storageBucket: "pruebas-onp.appspot.com",
+    messagingSenderId: "388875480802",
+    appId: "1:388875480802:web:adcc81491902c0f361f8bb",
+    measurementId: "G-CBGGP1GN2F"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
