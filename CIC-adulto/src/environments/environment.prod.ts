e5d6a28c1fea902b681807aad09429559ae171e0
jperez <filomenochahua@gmail.com>
2019-10-29 12:55:08 -0500

    V1.0.0 prod

--- CIC-adulto/src/environments/environment.prod.ts
+++ CIC-adulto/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCEYKDUQcKc4ruuSL-BJOeGiTaN2t69wxo",
+    authDomain: "pruebas-onp.firebaseapp.com",
+    databaseURL: "https://pruebas-onp.firebaseio.com",
+    projectId: "pruebas-onp",
+    storageBucket: "pruebas-onp.appspot.com",
+    messagingSenderId: "388875480802",
+    appId: "1:388875480802:web:adcc81491902c0f361f8bb",
+    measurementId: "G-CBGGP1GN2F"
+  }
 };
--- CIC-adulto/src/environments/environment.ts
+++ CIC-adulto/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: "AIzaSyCEYKDUQcKc4ruuSL-BJOeGiTaN2t69wxo",
     authDomain: "pruebas-onp.firebaseapp.com",
